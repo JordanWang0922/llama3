5ad86f071203e2362334626f7249e6c359e9a52a
JordanWang0922 <120356285+JordanWang0922@users.noreply.github.com>
2024-05-16 22:52:57 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -8,7 +8,9 @@ set -e
 read -p "Enter the URL from email: " PRESIGNED_URL
 echo ""
 read -p "Enter the list of models to download without spaces (8B,8B-instruct,70B,70B-instruct), or press Enter for all: " MODEL_SIZE
-TARGET_FOLDER="."             # where all files should end up
+echo ""
+read -p "Enter the target folder for downloading files (default is current directory): " TARGET_FOLDER
+TARGET_FOLDER=${TARGET_FOLDER:-"."}  # Use current directory if no input
 mkdir -p ${TARGET_FOLDER}
 
 if [[ $MODEL_SIZE == "" ]]; then
@@ -16,8 +18,8 @@ if [[ $MODEL_SIZE == "" ]]; then
 fi
 
 echo "Downloading LICENSE and Acceptable Usage Policy"
-wget --continue ${PRESIGNED_URL/'*'/"LICENSE"} -O ${TARGET_FOLDER}"/LICENSE"
-wget --continue ${PRESIGNED_URL/'*'/"USE_POLICY"} -O ${TARGET_FOLDER}"/USE_POLICY"
+curl -L -o ${TARGET_FOLDER}"/LICENSE" ${PRESIGNED_URL/'*'/"LICENSE"}
+curl -L -o ${TARGET_FOLDER}"/USE_POLICY" ${PRESIGNED_URL/'*'/"USE_POLICY"}
 
 for m in ${MODEL_SIZE//,/ }
 do
@@ -44,12 +46,12 @@ do
 
     for s in $(seq -f "0%g" 0 ${SHARD})
     do
-        wget --continue ${PRESIGNED_URL/'*'/"${MODEL_PATH}/consolidated.${s}.pth"} -O ${TARGET_FOLDER}"/${MODEL_FOLDER_PATH}/consolidated.${s}.pth"
+        curl -L -o ${TARGET_FOLDER}"/${MODEL_FOLDER_PATH}/consolidated.${s}.pth" ${PRESIGNED_URL/'*'/"${MODEL_PATH}/consolidated.${s}.pth"}
     done
 
-    wget --continue ${PRESIGNED_URL/'*'/"${MODEL_PATH}/params.json"} -O ${TARGET_FOLDER}"/${MODEL_FOLDER_PATH}/params.json"
-    wget --continue ${PRESIGNED_URL/'*'/"${MODEL_PATH}/tokenizer.model"} -O ${TARGET_FOLDER}"/${MODEL_FOLDER_PATH}/tokenizer.model"
-    wget --continue ${PRESIGNED_URL/'*'/"${MODEL_PATH}/checklist.chk"} -O ${TARGET_FOLDER}"/${MODEL_FOLDER_PATH}/checklist.chk"
+    curl -L -o ${TARGET_FOLDER}"/${MODEL_FOLDER_PATH}/params.json" ${PRESIGNED_URL/'*'/"${MODEL_PATH}/params.json"}
+    curl -L -o ${TARGET_FOLDER}"/${MODEL_FOLDER_PATH}/tokenizer.model" ${PRESIGNED_URL/'*'/"${MODEL_PATH}/tokenizer.model"}
+    curl -L -o ${TARGET_FOLDER}"/${MODEL_FOLDER_PATH}/checklist.chk" ${PRESIGNED_URL/'*'/"${MODEL_PATH}/checklist.chk"}
     echo "Checking checksums"
     CPU_ARCH=$(uname -m)
     if [[ "$CPU_ARCH" == "arm64" ]]; then
