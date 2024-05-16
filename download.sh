bd286c334ca8284f65227895ee612ca9a2b2afb0
JordanWang0922 <120356285+JordanWang0922@users.noreply.github.com>
2024-05-16 23:05:00 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,60 +1,63 @@
 #!/usr/bin/env bash
 
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
-
+# 设置脚本退出条件
 set -e
 
 read -p "Enter the URL from email: " PRESIGNED_URL
 echo ""
-read -p "Enter the list of models to download without spaces (8B,8B-instruct,70B,70B-instruct), or press Enter for all: " MODEL_SIZE
-TARGET_FOLDER="."             # where all files should end up
+
+read -p "Enter the list of models to download without spaces (8B,8B-instruct,7B,7B-instruct), or press Enter for all: " MODEL_SIZE
+echo ""
+
+# 设置目标文件夹路径
+TARGET_FOLDER="/e/AI_model_llama/llama3_model"
 mkdir -p ${TARGET_FOLDER}
 
 if [[ $MODEL_SIZE == "" ]]; then
-    MODEL_SIZE="8B,8B-instruct,70B,70B-instruct"
+    MODEL_SIZE="8B,8B-instruct,7B,7B-instruct"
 fi
 
 echo "Downloading LICENSE and Acceptable Usage Policy"
-wget --continue ${PRESIGNED_URL/'*'/"LICENSE"} -O ${TARGET_FOLDER}"/LICENSE"
-wget --continue ${PRESIGNED_URL/'*'/"USE_POLICY"} -O ${TARGET_FOLDER}"/USE_POLICY"
+curl -L -o ${TARGET_FOLDER}/LICENSE "${PRESIGNED_URL}/*/LICENSE"
+curl -L -o ${TARGET_FOLDER}/USE_POLICY.md "${PRESIGNED_URL}/*/USE_POLICY.md"
 
 for m in ${MODEL_SIZE//,/ }
 do
     if [[ $m == "8B" ]] || [[ $m == "8b" ]]; then
         SHARD=0
-        MODEL_FOLDER_PATH="Meta-Llama-3-8B"
+        MODEL_FOLDER_PATH="Meta-llama-3-8B"
         MODEL_PATH="8b_pre_trained"
-    elif [[ $m == "8B-instruct" ]] || [[ $m == "8b-instruct" ]] || [[ $m == "8b-Instruct" ]] || [[ $m == "8B-Instruct" ]]; then
+    elif [[ $m == "8B-instruct" ]] || [[ $m == "8B-Instruct" ]] || [[ $m == "8b-instruct" ]]; then
         SHARD=0
-        MODEL_FOLDER_PATH="Meta-Llama-3-8B-Instruct"
+        MODEL_FOLDER_PATH="Meta-llama-3-8B-Instruct"
         MODEL_PATH="8b_instruction_tuned"
-    elif [[ $m == "70B" ]] || [[ $m == "70b" ]]; then
+    elif [[ $m == "7B" ]] || [[ $m == "7b" ]]; then
         SHARD=7
-        MODEL_FOLDER_PATH="Meta-Llama-3-70B"
-        MODEL_PATH="70b_pre_trained"
-    elif [[ $m == "70B-instruct" ]] || [[ $m == "70b-instruct" ]] || [[ $m == "70b-Instruct" ]] || [[ $m == "70B-Instruct" ]]; then
+        MODEL_FOLDER_PATH="Meta-llama-3-7B"
+        MODEL_PATH="7b_pre_trained"
+    elif [[ $m == "7B-instruct" ]] || [[ $m == "7B-Instruct" ]] || [[ $m == "7b-instruct" ]]; then
         SHARD=7
-        MODEL_FOLDER_PATH="Meta-Llama-3-70B-Instruct"
-        MODEL_PATH="70b_instruction_tuned"
+        MODEL_FOLDER_PATH="Meta-llama-3-7B-Instruct"
+        MODEL_PATH="7b_instruction_tuned"
     fi
 
     echo "Downloading ${MODEL_PATH}"
-    mkdir -p ${TARGET_FOLDER}"/${MODEL_FOLDER_PATH}"
+    mkdir -p ${TARGET_FOLDER}/${MODEL_FOLDER_PATH}
 
-    for s in $(seq -f "0%g" 0 ${SHARD})
+    for s in $(seq -f "%03g" 0 ${SHARD})
     do
-        wget --continue ${PRESIGNED_URL/'*'/"${MODEL_PATH}/consolidated.${s}.pth"} -O ${TARGET_FOLDER}"/${MODEL_FOLDER_PATH}/consolidated.${s}.pth"
+        curl -L -o ${TARGET_FOLDER}/${MODEL_FOLDER_PATH}/consolidated.${s}.pth "${PRESIGNED_URL}/*/${MODEL_PATH}/consolidated.${s}.pth"
     done
 
-    wget --continue ${PRESIGNED_URL/'*'/"${MODEL_PATH}/params.json"} -O ${TARGET_FOLDER}"/${MODEL_FOLDER_PATH}/params.json"
-    wget --continue ${PRESIGNED_URL/'*'/"${MODEL_PATH}/tokenizer.model"} -O ${TARGET_FOLDER}"/${MODEL_FOLDER_PATH}/tokenizer.model"
-    wget --continue ${PRESIGNED_URL/'*'/"${MODEL_PATH}/checklist.chk"} -O ${TARGET_FOLDER}"/${MODEL_FOLDER_PATH}/checklist.chk"
+    curl -L -o ${TARGET_FOLDER}/${MODEL_FOLDER_PATH}/params.json "${PRESIGNED_URL}/*/${MODEL_PATH}/params.json"
+    curl -L -o ${TARGET_FOLDER}/${MODEL_FOLDER_PATH}/tokenizer.model "${PRESIGNED_URL}/*/${MODEL_PATH}/tokenizer.model"
+    curl -L -o ${TARGET_FOLDER}/${MODEL_FOLDER_PATH}/checklist.chk "${PRESIGNED_URL}/*/${MODEL_PATH}/checklist.chk"
+
     echo "Checking checksums"
     CPU_ARCH=$(uname -m)
-    if [[ "$CPU_ARCH" == "arm64" ]]; then
-      (cd ${TARGET_FOLDER}"/${MODEL_FOLDER_PATH}" && md5 checklist.chk)
+    if [[ $CPU_ARCH == "arm64" ]]; then
+        (cd ${TARGET_FOLDER}/${MODEL_FOLDER_PATH} && md5 checklist.chk)
     else
-      (cd ${TARGET_FOLDER}"/${MODEL_FOLDER_PATH}" && md5sum -c checklist.chk)
+        (cd ${TARGET_FOLDER}/${MODEL_FOLDER_PATH} && md5sum -c checklist.chk)
     fi
 done
